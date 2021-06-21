1cdf562b5490a3eb7e8e6720b8ec0b1d8d2ce90e
sans-connaissance <davemalicke@gmail.com>
2021-06-21 09:57:22 -0400

    Initial Commit

---
+++ Shared/RW_NetworkingLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RW_NetworkingLessonsApp.swift
+//  Shared
+//
+//  Created by David Malicke on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct RW_NetworkingLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
