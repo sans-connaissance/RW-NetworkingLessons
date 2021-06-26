192a2428148fae26cc80fe17259ede8621868ded
sans-connaissance <davemalicke@gmail.com>
2021-06-26 11:37:33 -0400

    Updated base content view message.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Check out a branch to review URL Session excercise examples.")
             .padding()
     }
 }
